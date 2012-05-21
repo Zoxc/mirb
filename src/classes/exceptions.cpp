af165121d20e56e20ee54c84538d18bdac71c341
Zoxc <zoxc32@gmail.com>
2012-05-21 02:10:52 +0200

    Fixed a typo.

--- src/classes/exceptions.cpp
+++ src/classes/exceptions.cpp
@@ -14,7 +14,7 @@ namespace Mirb
 		context->runtime_error = define_class("RuntimeError", context->standard_error);
 		context->local_jump_error = define_class("LocalJumpError", context->standard_error);
 		context->load_error = define_class("LoadError", context->standard_error);
-		context->system_call_error = define_class("SysemCallError", context->standard_error);
+		context->system_call_error = define_class("SystemCallError", context->standard_error);
 		
 		context->signal_exception = define_class("SignalException", context->exception_class);
 		context->interrupt_class = define_class("Interrupt", context->signal_exception);
