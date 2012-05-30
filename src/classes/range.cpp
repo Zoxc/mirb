3a24d2fa9333c4338768ecafd8bfc882555feb7d
Zoxc <zoxc32@gmail.com>
2012-05-30 06:40:17 +0200

    Added Range#=== as an alias for include? and made sure include? doesn't raise exceptions.

--- src/classes/range.cpp
+++ src/classes/range.cpp
@@ -101,13 +101,22 @@ namespace Mirb
 	{
 		OnStack<2> os(self, value);
 
-		if(compare(self->low, value) > 0)
-			return value_false;
+		try
+		{
+			if(compare(self->low, value) > 0)
+				return value_false;
 
-		if(self->flag)
-			return Value::from_bool(compare(self->high, value) > 0);
-		else
-			return Value::from_bool(compare(self->high, value) >= 0);
+			if(self->flag)
+				return Value::from_bool(compare(self->high, value) > 0);
+			else
+				return Value::from_bool(compare(self->high, value) >= 0);
+		} catch(const InternalException &e)
+		{
+			if(!Mirb::kind_of(context->standard_error, e.value))
+				throw;
+
+			return value_false;
+		}
 	}
 	
 	void Range::initialize()
@@ -115,6 +124,7 @@ namespace Mirb
 		context->range_class = define_class("Range", context->object_class);
 		
 		method<Arg::Self<Arg::Class<Range>>, Arg::Value, &include>(context->range_class, "include?");
+		method<Arg::Self<Arg::Class<Range>>, Arg::Value, &include>(context->range_class, "===");
 		method<Arg::Self<Arg::Class<Range>>, &exclude_end>(context->range_class, "exclude_end?");
 		method<Arg::Self<Arg::Class<Range>>, &first>(context->range_class, "first");
 		method<Arg::Self<Arg::Class<Range>>, &last>(context->range_class, "last");
